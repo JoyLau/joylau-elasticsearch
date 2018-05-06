c4ab7da21e8de484539408f84dc6fad0ae576a04
liufa <2587038142@qq.com>
2018-05-06 08:27:22 +0800

    add logback config

--- src/main/java/cn/joylau/code/joylau/JoylauElasticsearchApplication.java
+++ src/main/java/cn/joylau/code/joylau/JoylauElasticsearchApplication.java
@@ -1,13 +1,23 @@
 package cn.joylau.code.joylau;
 
 import lombok.AllArgsConstructor;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @AllArgsConstructor
 public class JoylauElasticsearchApplication {
+	private static final Logger LOGGER = Logger.getLogger(JoylauElasticsearchApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(JoylauElasticsearchApplication.class, args);
+		for (int i = 0; i < 10; i++) {
+			LOGGER.error("Info log [" + i + "].");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
